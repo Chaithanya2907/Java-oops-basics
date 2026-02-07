da485b88c3e278eee8ff21e3cc3745272c7a1144
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-02-07 22:27:30 +0530

    Added C program to multiply two integers.

---
+++ mul.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ void main()
+{
+int a,b;
+printf("Enter an first integer:\n");
+scanf("%d",&a);
+printf("enter an second integer:\n");
+scanf("%d",&b);
+printf("total answer is:\n");
+printf("%d",a/b);
+}
