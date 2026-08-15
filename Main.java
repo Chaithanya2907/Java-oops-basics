0204cc01f0be7054425782d5e773e29f57a8f9ca
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-08-15 16:41:03 +0530

    Added shopping discount calculator with price and quantity based discounts.

--- Main.java
+++ Main.java
@@ -1,29 +1,41 @@
-class Student
-{
-	String name;
-   	int marks;
-	int id;
-	public Student(String name, int marks, int id)
-   	{
-		this.name=name;
-     		this.id=id;
-		this.marks=marks;
-	}
-	void display()
-	{
-		System.out.println("Name of the student:" + name);
-     		System.out.println("id:" + id);
-		System.out.println("Marks:" + marks);
-   	}
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        double product_price;
+        int quantity;
+        double total;
+        double discount;
+        double final_price;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter product price: ");
+        product_price = s.nextDouble();
+
+        System.out.print("Enter quantity of product: ");
+        quantity = s.nextInt();
+
+        total = product_price * quantity;
+
+        if (total >= 5000) {
+            discount = total * 0.20;
+        }
+        else if (total >= 3000) {
+            discount = total * 0.10;
+        }
+        else if (total >= 1000) {
+            discount = total * 0.05;
+        }
+        else {
+            discount = 0;
+        }
+
+        final_price = total - discount;
+
+        System.out.println("Total price: " + total);
+        System.out.println("Discount: " + discount);
+        System.out.println("Final price: " + final_price);
+    }
 }
-public class Main
-{
-	public static void main(String args[])
-   	{
-		Student s=new Student("chai",8,98);
-     		s.display();
-	}
-}	
-	
-	
- 
