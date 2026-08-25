9b43e3afe040c7bc8565a64147454be62776b567
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-08-25 23:04:55 +0530

    Added Java movie ticket booking system with age-based discounts and methods.

--- main1.java
+++ main1.java
@@ -1,27 +1,57 @@
-import java.util.Scanner;
-public class main1{
-    public static void main(String args[]){
-        int PIN=1234;
-        int p;
-        Scanner sc=new Scanner(System.in);
-        for(int i=0;i<3;i++){
-            System.out.println("Enter the PIN:");
-             p=sc.nextInt();
-            if (p==PIN){
-                System.out.println("PIN correct!");
-                System.out.println("Welcome to ATM");
-                break;
-            }
-            else if (p!=PIN)
-                {
-                System.out.println("Incorrect PIN!");
-                if (i==2){
-                  System.out.println("Your card is blocked!");
-            }
-            else{
-                System.out.println("Attempts remaining:" +(2-i));
-            }
-        }
+ import java.util.Scanner;
+
+public class Main1 {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        int age;
+        int tickets;
+        int total;
+
+        System.out.print("Enter name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter age: ");
+        age = sc.nextInt();
+
+        System.out.print("Enter tickets: ");
+        tickets = sc.nextInt();
+
+        total = calculate_amount(age, tickets);
+
+        System.out.println("\n~~~~~ TICKET DETAILS ~~~~~");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Tickets: " + tickets);
+        System.out.println("Total: ₹" + (tickets * 200));
+        System.out.println("Final amount: ₹" + total);
+
+        sc.close();
     }
-}
+
+    static int calculate_amount(int age, int tickets) {
+
+        int total = tickets * 200;
+
+        if (age < 5) {
+            System.out.println("Discount: 100%");
+            total = 0;
+        }
+        else if (age >= 5 && age <= 12) {
+            System.out.println("Discount: 50%");
+            total = total / 2;
+        }
+        else if (age >= 13 && age <= 59) {
+            System.out.println("Discount: 0%");
+        }
+        else {
+            System.out.println("Discount: 30%");
+            total = total - (total * 30 / 100);
+        }
+
+        return total;
+    }
 }
