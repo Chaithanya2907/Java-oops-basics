496056f7682870f5ccea3cbaa08b5e740a1562fe
Chaithanya2907 <chaithanayachaithu83@gmail.com>
2026-08-28 22:47:06 +0530

    Added electricity bill calculator using methods and do-while loop.

--- Main.java
+++ Main.java
@@ -1,44 +1,49 @@
- import java.util.Scanner;
-
-public class Main {
-
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name;
+        int unit;
+        int choice;
+        int bill;
+        do {
+    
+        System.out.print("Enter customer name:");
+        name=sc.nextLine();
+        System.out.print("Enter units consumed:");
+        unit=sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-
-        int num;
-        int positive = 0;
-        int negative = 0;
-        int zero = 0;
-        int sum = 0;
-
-        for (int i = 0; i < 10; i++) {
-
-            System.out.print("Enter number " + (i + 1) + ": ");
-            num = sc.nextInt();
-
-            sum = sum + num;
-
-            if (num > 0) {
-                positive++;
-            }
-            else if (num < 0) {
-                negative++;
-            }
-            else {
-                zero++;
-            }
-        }
-
-        double average = sum / 10.0;
-
-        System.out.println("\n===== NUMBER ANALYSIS =====");
-        System.out.println("Positive numbers: " + positive);
-        System.out.println("Negative numbers: " + negative);
-        System.out.println("Zeros: " + zero);
-        System.out.println("Sum: " + sum);
-        System.out.println("Average: " + average);
-
-        sc.close();
+        bill=calculateBill(unit); 
+        System.out.println("=====ELECTRICITY BILL=====");
+    System.out.println("Customer:" + name);
+    System.out.println("units consumed:" + unit);
+    System.out.println("Bill amount:" + bill);
+    System.out.println("=====ELECTRICITY BILL=====");
+    sc.nextLine();
+    System.out.println("1. Calculate another bill");
+System.out.println("2. Exit");
+System.out.print("Enter your choice: ");
+choice = sc.nextInt();
+sc.nextLine();
+        } 
+        while(choice==1);  
     }
+     static int calculateBill(int unit){
+        int bill=0;
+        if(unit>0 && unit<=100){
+            bill=unit*2;
+        }
+        else if(unit>=101 && unit<=200){
+            bill=unit*3;
+        }
+        else if(unit>=201 && unit<=300){
+            bill=unit*5;
+        }
+        else if (unit>300){
+            bill=unit*7;
+        }
+            return bill;
+        }
+     
+    
 }
